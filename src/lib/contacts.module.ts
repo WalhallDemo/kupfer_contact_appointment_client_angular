d2c8b0391d65e980b949843db6781e0ff1474a87
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-09-24 16:04:09 +0200

    fix contacts module

--- src/lib/contacts.module.ts
+++ src/lib/contacts.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ContactsComponent } from './contacts.component';
 import { ContactsRoutingModule } from '@libs/contacts/src/lib/contacts.routing-module';
-import { MidgardHttpModule } from '@libs/midgard-angular/src/lib/modules/http-module/http.module';
-import { MigardStoreModule } from '@libs/midgard-angular/src/lib/modules/store-module/store.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
